b214358b8fe7dcc71f4b25c0465607e271b7d124
Dell Home <pon.felipon@gmail.com>
2021-09-24 13:07:15 -0700

    Adding the recipes search button, and functionality

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,9 @@
 from webapp import app
 from flask import render_template, flash, request, jsonify
+from helpers import get_recipes_api
 import requests
 import json
+import random
 
 # Api url
 apiUrl = 'https://world.openfoodfacts.org/api/v0/product/'
@@ -13,9 +15,12 @@ def index():
 @app.route('/query', methods=['GET', 'POST'])
 def query():
 	if request.method == 'POST':
+		query = request.form['query']
 		response = requests.get(apiUrl + request.form['query'] + '.json')
 		data = json.loads(response.text)
 		ingredients = []
+		stop_words = ['food', 'their', 'america', 'value', 'great', 'base', 'fabrique', 'dell', 'non-ue', 'nl-bio-01', 'sale', 'salee']
+		word = ''
 
 		if data['status'] == 1:
 			if ('ingredients' not in data['product']) or (data['product'].get('ingredients') == []):
@@ -23,8 +28,9 @@ def query():
 				data['status_verbose'] = 'Oh Snap!, there are no ingredients in this product'
 			else: 
 				ingredients = data['product']['ingredients']
+				word = random.choice( [word for word in data['product']['_keywords'] if (word not in stop_words) and ( len(word) > 3)])
 
-		return render_template('table.html', data=data, ingredients=ingredients ) 
+		return render_template('table.html', data=data, ingredients=ingredients, word=word ) 
 
 	else: 
 		return 'error', 505
@@ -32,4 +38,10 @@ def query():
 
 @app.route('/scanner', methods=['GET', 'POST'])
 def scanner():
-	return render_template('scanner.html')
+	return render_template('modal.html')
+
+@app.route('/recipes/<ingredient>', methods=['GET', 'POST'])
+def recipes(ingredient):
+	print(ingredient)
+	recipes = get_recipes_api(ingredient)
+	return render_template('recipes.html', recipes=recipes )
